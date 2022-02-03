91a28b7e31577a83826cf685930399338338050a
Alejandro Nasarre <alu2258@ieselcaminas.org>
2022-02-03 20:15:43 +0100

    mudanzas

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         Bulto objeto1 = new Bulto(1, 5, 5);
         Bulto objeto2 = new Bulto(2, 5, 5);
         Bulto objeto3 = new Bulto(3, 5, 5);
-        Bulto objeto4 = new Bulto(3, 5, 5);
-        Bulto objeto5 = new Bulto(3, 5, 5);
+        Bulto objeto4 = new Bulto(4, 5, 5);
+        Bulto objeto5 = new Bulto(5, 5, 5);
         Camion camion1 = new Camion("123232F", 10, 10);
         Mudanza mudanza1 = new Mudanza(1, 20, camion1);
         mudanza1.addBulto(objeto1);
